2d6a3861ee29bb1fc6e0d495e1c80203d2efd78a
Olya Antonovich <ynonaj@gmail.com>
2022-04-01 23:06:24 +0300

    Р”РѕРІР°Р»РµРЅС‹ РїСЂРѕРІРµСЂРєРё СЃРѕСЂС‚РёСЂРѕРІРѕРє

--- home_work_2/sorts/SortsMain.java
+++ home_work_2/sorts/SortsMain.java
@@ -17,7 +17,50 @@ public class SortsMain {
         Arrays.toString(x);
         System.out.println("Массив после сортировки:" + Arrays.toString(x));
 
+        int [] y = {1,1,1,1};
+
+        Arrays.toString(y);
+        System.out.println("Массив до сортировки:" + Arrays.toString(y));
+
+        SortsUtils.buble (y);
+
+        Arrays.toString(y);
+        System.out.println("Массив после сортировки:" + Arrays.toString(y));
+
+        int [] m = {9,1,5,99,9,9};
+
+        Arrays.toString(m);
+        System.out.println("Массив до сортировки:" + Arrays.toString(m));
+
+        SortsUtils.buble (m);
+
+        Arrays.toString(m);
+        System.out.println("Массив после сортировки:" + Arrays.toString(m));
+
+        int [] z = {};
+
+        Arrays.toString(z);
+        System.out.println("Массив до сортировки:" + Arrays.toString(z));
+
+        SortsUtils.buble (z);
+
+        Arrays.toString(z);
+        System.out.println("Массив после сортировки:" + Arrays.toString(z));
+
+        int [] e = {6,5,4,3,2,1};
+
+        Arrays.toString(e);
+        System.out.println("Массив до сортировки:" + Arrays.toString(e));
+
+        SortsUtils.buble (e);
+
+        Arrays.toString(e);
+        System.out.println("Массив после сортировки:" + Arrays.toString(e));
+
+
     }
+
+
 
 
 
