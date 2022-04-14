40553ef78c2ef53927d252d1a923ef1025c0872e
Olya Antonovich <ynonaj@gmail.com>
2022-04-14 21:46:27 +0300

    Р”РѕР±Р°РІР»РµРЅРѕ 10-Рµ Р·Р°РґР°РЅРёРµ

---
+++ home_work_3/calcs/additional/CalculatorWithMemory.java
@@ -0,0 +1,171 @@
+package home_work_3.calcs.additional;
+
+import home_work_3.calcs.api.ICalculator;
+
+import java.util.ArrayList;
+
+public class CalculatorWithMemory {
+
+    private ICalculator icalculator;
+
+    public CalculatorWithMemory(ICalculator icalculator){
+
+        this.icalculator = icalculator;
+    }
+
+    private long count;
+    double a;
+    double b;
+    /**
+     * создаем автоматически расширяемый массив ArrayList
+     */
+    private ArrayList <Double> memories = new ArrayList();
+    double memory;
+
+
+
+    public void setAB (double a,double b){
+
+        icalculator.setAB(a, b);
+    }
+
+    /**
+     * метод, результат работы которого произведение двух чисел
+     * count -счетчик
+     * memories.add(icalculator.getProizv()) - добавление объекта в массив ArrayList
+     * @return произведение двух чисел
+     */
+
+    public double getProizv() {
+        count++;
+        memories.add(icalculator.getProizv());
+
+         return icalculator.getProizv();
+    }
+
+    /**
+     * метод, результат работы которого частное от деления двух чисел
+     * count -счетчик
+     * memories.add(icalculator.getDel()) - добавление объекта в массив ArrayList
+     * @return частное от деления двух чисел
+     */
+
+
+    public double getDel() {
+        count++;
+        memories.add(icalculator.getDel());
+        return icalculator.getDel();
+    }
+
+    public void setFD(double f, int d) {
+
+        icalculator.setFD(f, d);
+
+    }
+
+    /**
+     * метод, результат работы которого возведение в степень числа
+     * count -счетчик
+     * memories.add(icalculator.getStep()) - добавление объекта в массив ArrayList
+     * @return возведение в степень числа
+     */
+
+    public double getStep() {
+        count++;
+        memories.add(icalculator.getStep());
+       return icalculator.getStep();
+
+    }
+
+    /**
+     * метод, результат работы которого сумма двух чисел
+     * count -счетчик
+     * memories.add(icalculator.getSumm()) - добавление объекта в массив ArrayList
+     * @return сумма двух чисел
+     */
+
+    public double getSumm() {
+        count++;
+        memories.add(icalculator.getSumm());
+        return icalculator.getSumm();
+    }
+
+
+    public void setC (double c) {
+
+        icalculator.setC(c);
+    }
+
+    /**
+     * метод, результат работы которого разница двух чисел
+     * count -счетчик
+     * memories.add(icalculator.getSubtract()) - добавление объекта в массив ArrayList
+     * @return сумма двух чисел
+     */
+
+    public double getSubtract() {
+        count++;
+        memories.add(icalculator.getSubtract());
+        return icalculator.getSubtract();
+    }
+    /**
+     * метод, результат работы которого модуль числа
+     * count -счетчик
+     * memories.add(icalculator.getAbs()) - добавление объекта в массив ArrayList
+     * @return модуль числа
+     */
+
+    public double getAbs() {
+        count++;
+
+        return icalculator.getAbs();
+
+    }
+    /**
+     * метод, результат работы которого квадратный корень из  числа
+     * count -счетчик
+     * memories.add(icalculator.getAbs()) - добавление объекта в массив ArrayList
+     * @return квадратный корень из  числа
+     */
+
+
+    public double getSqrt() {
+        count++;
+        memories.add(icalculator.getSqrt());
+        return icalculator.getSqrt();
+
+    }
+
+    /**
+     * метод, который считает количество вызовов математической функции
+     * @return количество вызовов математической функции
+     */
+
+        public long getCountOperation (){
+
+            return count;
+        }
+
+    /**
+     * метод, который записывает в память результат работы
+     * последней вызванной математической функции,
+     * вызывая последний элемент массива ArrayList
+     */
+        public void Memory () {
+
+            memory = memories.get(memories.size() - 1);
+               }
+
+    /**
+     * метод, который вызывает из памяти последнее записанное в нее значение
+     * @return последнее записанное в память значение
+     */
+    public double getMemory (){
+        return memory;
+    }
+
+    }
+
+
+
+
---
+++ home_work_3/runners/CalculatorWhithMemoryMain.java
@@ -0,0 +1,72 @@
+package home_work_3.runners;
+
+
+
+import home_work_3.calcs.additional.CalculatorWithMemory;
+import home_work_3.calcs.api.ICalculator;
+import home_work_3.calcs.simple.CalculatorWithOperator;
+
+import java.text.DecimalFormat;
+
+
+/**
+ * Посчитать выражение 4.1+ 15*7+(28/5)^2
+ * и вывести сохраненный результат в консоль
+ */
+
+
+public class CalculatorWhithMemoryMain {
+
+    public static void main(String[] args) {
+
+
+        ICalculator icalculator = new CalculatorWithOperator ();
+        CalculatorWithMemory calculator = new CalculatorWithMemory (icalculator);
+
+        calculator.setAB(15,7);
+        calculator.getProizv();
+        calculator.Memory();
+
+        System.out.println(calculator.getMemory());
+
+        calculator.setAB(28,5);
+        calculator.getDel();
+
+        calculator.setFD(calculator.getDel(),2);
+        calculator.getStep();
+
+        calculator.setAB(calculator.getStep(), calculator.getMemory());
+        calculator.getSumm();
+
+        System.out.println(calculator.getSumm());
+        calculator.Memory();
+
+        calculator.setAB(4.1, calculator.getMemory());
+
+        DecimalFormat dr = new DecimalFormat ("#.##");
+
+        System.out.println("результат выражения:4.1+ 15*7+(28/5)^2 = " + dr.format(calculator.getSumm()));
+
+        System.out.println("количество вызовов математических операций = " +  calculator.getCountOperation());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
